f9e212e379d94d22b7836ba22ac6b598bcd17712
Zhong Wang <zhonwang@seas.upenn.edu>
2017-01-25 23:14:31 -0500

    Finish getopt_experiment.cc

--- getopt_experiment.cc
+++ getopt_experiment.cc
@@ -10,14 +10,44 @@ int main(int argc, char * argv[])
 {
 	// use this varible to save the return value 
 	// form getopt().
-	int option;
+	int option = -1;
 
 	// set opterr to 0 ot prevent getopt() sending
 	// error msg to stderr.
 	opterr = 0;
 	
-	while((option = getopt(argc, argv, "n:")) != -1)
+	while((option = getopt(argc, argv, ":tn:")) != -1)
 	{
-		
+		switch(option)
+		{
+			case 'n':
+				printf("case n\n");
+				printf("N = %s\n", optarg);
+				break;
+			case 't':
+				printf("case t\n");
+				printf("optarg: %s\n", optarg);
+				printf("optopt: %c\n", optopt);
+				break;
+			case '?':
+				printf("case ?\n");
+				printf("optarg: %s\n", optarg);
+				printf("optopt: %c\n", optopt);		
+				break;
+			default:
+				exit(1);
+		}
 	}
+	printf("argc: %d\n", argc);
+	printf("optind: %d\n", optind);
+	printf("argv:\n");
+	for(int i=0; i < argc; i++) 
+	{
+		printf("%s", argv[i]);
+		if(i != argc-1)
+			printf("->");
+		else
+			printf("\n");
+	}
+	return 0;
 }
